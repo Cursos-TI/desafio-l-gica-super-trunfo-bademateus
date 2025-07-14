37d3e87a9d15d16d09f85a68f867687dc9340daa
bademateus <mateus.bade@hotmail.com>
2025-07-14 00:31:30 +0000

    Desafio Super Trunfo COMPLETO

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
@@ -6,9 +7,12 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+     // ======= DECLARAÇÃO DAS VARIÁVEIS ======
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
+  char nomeCidade1[50], nomeCidade2[50];
+    int populacao1, populacao2;
+    float area1, area2, pib1, pib2;
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -20,10 +24,41 @@ int main() {
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
+     // ======= ENTRADA DE DADOS - CIDADE 1 =======
+     printf("Digite o nome da Cidade 1: ");
+      fgets(nomeCidade1, sizeof(nomeCidade1), stdin);
+    nomeCidade1[strcspn(nomeCidade1, "\n")] = '\0';
+
+    printf("Digite a população da Cidade 1: ");
+    scanf("%d", &populacao1);
+
+     printf("Digite a área da Cidade 1 (em km²): ");
+    scanf("%f", &area1);
+
+   printf("Digite o PIB da Cidade 1 (em bilhões): ");
+    scanf("%f", &pib1);
+    getchar();
+    
+     // ======= ENTRADA DE DADOS - CIDADE 2 =======
+
+    printf("\nDigite o nome da Cidade 2: ");
+    fgets(nomeCidade2, sizeof(nomeCidade2), stdin);
+    nomeCidade2[strcspn(nomeCidade2, "\n")] = '\0';
+
+        printf("Digite a população da Cidade 2: ");
+    scanf("%d", &populacao2);
+
+       printf("Digite a área da Cidade 2 (em km²): ");
+    scanf("%f", &area2);
+
+   printf("Digite o PIB da Cidade 2 (em bilhões): ");
+    scanf("%f", &pib2);
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+
+      printf("\n=== COMPARAÇÃO DE ATRIBUTOS ===\n");
 
     // Exemplo:
     // if (populacaoA > populacaoB) {
@@ -31,6 +66,31 @@ int main() {
     // } else {
     //     printf("Cidade 2 tem maior população.\n");
     // }
+
+    // Comparar População
+    if (populacao1 > populacao2)
+        printf("População: %s vence!\n", nomeCidade1);
+    else if (populacao2 > populacao1)
+        printf("População: %s vence!\n", nomeCidade2);
+    else
+        printf("População: Empate!\n");
+
+    // Comparar Área
+     if (area1 > area2)
+        printf("Área: %s vence!\n", nomeCidade1);
+    else if (area2 > area1)
+        printf("Área: %s vence!\n", nomeCidade2);
+    else
+        printf("Área: Empate!\n");
+
+// Comparar PIB
+ if (pib1 > pib2)
+        printf("PIB: %s vence!\n", nomeCidade1);
+    else if (pib2 > pib1)
+        printf("PIB: %s vence!\n", nomeCidade2);
+    else
+        printf("PIB: Empate!\n");
+    
 
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
